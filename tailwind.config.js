f82f4fae4f0e595d9ceabcc5a2eb637782dc8105
shenderaj9021 <rajshende220802@gmail.com>
2021-12-08 00:36:55 +0530

    commit after custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         secondary: {
           100: '#E2E2D5',
           200 : '#888883',
+        },
+        fontFamily:{
+          body: ['Nunito']
         }
       }
     },
